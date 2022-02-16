e92f3062830b395701ce7227cb4e736e36d9bce8
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-16 14:57:59 +0100

    turning animation added but failed

--- HalloweenGameJam2021/Assets/Scripts/Player/MoveController.cs
+++ HalloweenGameJam2021/Assets/Scripts/Player/MoveController.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
+    [SerializeField] private Animator myAnimator;
     [SerializeField] private SpriteRenderer mySpriteRenderer;
     [SerializeField] private CommandContainer myCommandContainer;
     [SerializeField] private Rigidbody2D myRigidBody2D;
     public float moveSpeed = 5f;
+    [SerializeField] private float animationCounter;
+    [SerializeField] private float animationTime = 0.1f;
+    [SerializeField] private bool lastInput;
+
+    private void Start()
+    {
+        lastInput = true;
+    }
+
     void Update()
     {
+        //turnAnim();
         if (myCommandContainer.moveCommand != 0)
         {
             myRigidBody2D.velocity = new Vector2(moveSpeed * myCommandContainer.moveCommand, myRigidBody2D.velocity.y);
@@ -23,6 +35,48 @@ public class MoveController : MonoBehaviour
         if (myCommandContainer.moveCommand < 0)
         {
             mySpriteRenderer.flipX = false;
+        }
+    }
+
+    void turnAnim()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                lastInput = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                lastInput = false;
+            }
+
+            if (!lastInput)
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    Debug.Log("turn");
+                }
+                myAnimator.SetTrigger("Turning");
+                animationCounter -= Time.deltaTime;
+                if (animationCounter < 0)
+                {
+                    animationCounter = animationTime;
+                    myAnimator.SetTrigger("StoppedTurning");
+                }
+            }
+            if (myCommandContainer.moveCommand > 0 && lastInput)
+            {
+                Debug.Log("turn");
+                myAnimator.SetTrigger("Turning");
+                animationCounter -= Time.deltaTime;
+                if (animationCounter < 0)
+                {
+                    animationCounter = animationTime;
+                    myAnimator.SetTrigger("StoppedTurning");
+                }
+            }
         }
     }
 }
