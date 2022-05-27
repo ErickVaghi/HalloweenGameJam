d0ceb7b61152efeb462efc44bfe5910745b9a769
Arvid Lundin <arvid.lundin@student.forsbergsskola.se>
2022-05-27 14:45:41 +0200

    Fixed firedAudio

--- HalloweenGameJam2021/Assets/FireAudio.cs
+++ HalloweenGameJam2021/Assets/FireAudio.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FireAudio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public FMODUnity.EventReference FireRef;
+    private FMOD.Studio.EventInstance FireInst;
+
+
+
+    private void Start()
     {
-        
+
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        FireInst = FMODUnity.RuntimeManager.CreateInstance(FireRef);
+        FireInst.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        Debug.Log("TriggerFireAudio");
+        FireInst.start();
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("ReleaseFireAudio");
+        FireInst.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        FireInst.release();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
