0b925715bbf000dacb7f5fad80d167abd4eeea5f
Arvid Lundin <arvid.lundin@student.forsbergsskola.se>
2022-07-06 13:15:16 +0200

    build to unity

--- HalloweenGameJam2021/Assets/TrapAudio.cs
+++ HalloweenGameJam2021/Assets/TrapAudio.cs
@@ -12,33 +12,21 @@ public class TrapAudio : MonoBehaviour
     {
         TrapInst = FMODUnity.RuntimeManager.CreateInstance(TrapRef);
         TrapInst.getPlaybackState(out pbState);
-        TrapInst.start();
+        if (pbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            TrapInst.start();
+        }
+
     }
 
     // Update is called once per frame
+
     void Update()
     {
         TrapInst.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+
     }
-    //public void OnTriggerEnter2D(Collider2D collision)
-    //{
-
-        //if (collision.tag == ("Player") && pbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
-        //{
-            //TrapInst.start();
-            //Debug.Log("StartTrapSound");
-        //}
-        //else
-        //{
-
-       // }
 
 
-   // }
-    //public void OnTriggerExit2D(Collider2D collision)
-    //{
-        //TrapInst.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        //TrapInst.release();
-        //Debug.Log("StopTrapSound");
-   // }
+
 }
